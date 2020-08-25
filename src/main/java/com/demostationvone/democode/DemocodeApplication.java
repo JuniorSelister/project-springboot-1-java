1165d80f6835a26ac0475ec0248b536ddc08e8e2
Junior Selister <junior.selister@gmail.com>
2020-08-24 22:32:44 -0300

    Project created

---
+++ src/main/java/com/demostationvone/democode/DemocodeApplication.java
@@ -0,0 +1,13 @@
+package com.demostationvone.democode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demostationvone/democode/DemocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demostationvone.democode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
