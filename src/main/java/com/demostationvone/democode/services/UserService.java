2876300092b1ff54b0854d7bede9480e52eed70f
Junior Selister <junior.selister@gmail.com>
2020-08-29 14:29:16 -0300

    Service layer, component registration

---
+++ src/main/java/com/demostationvone/democode/services/UserService.java
@@ -0,0 +1,26 @@
+package com.demostationvone.democode.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demostationvone.democode.entities.User;
+import com.demostationvone.democode.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
