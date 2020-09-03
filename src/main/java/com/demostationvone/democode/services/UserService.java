61d311b1b333130d5638c762b8ec078441d376c9
Junior Selister <junior.selister@gmail.com>
2020-09-03 00:13:38 -0300

    Exception handling - delete

--- src/main/java/com/demostationvone/democode/services/UserService.java
+++ src/main/java/com/demostationvone/democode/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.demostationvone.democode.entities.User;
 import com.demostationvone.democode.repositories.UserRepository;
+import com.demostationvone.democode.services.exceptions.DatabaseException;
 import com.demostationvone.democode.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {			
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()); 
+		}
 	}
 	
 	public User update(Long id, User obj) {
