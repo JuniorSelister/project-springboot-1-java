a524348491b30ea42c7be9a2ade0532ff4aa21cb
Junior Selister <junior.selister@gmail.com>
2020-09-02 21:23:13 -0300

    Payment, one-to-one association

--- src/main/java/com/demostationvone/democode/config/TestConfig.java
+++ src/main/java/com/demostationvone/democode/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.demostationvone.democode.entities.Category;
 import com.demostationvone.democode.entities.Order;
 import com.demostationvone.democode.entities.OrderItem;
+import com.demostationvone.democode.entities.Payment;
 import com.demostationvone.democode.entities.Product;
 import com.demostationvone.democode.entities.User;
 import com.demostationvone.democode.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
